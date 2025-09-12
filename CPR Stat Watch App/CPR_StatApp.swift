81a45978e6030be13b3d282b338806f1be6968e1
jacknaylordunn <jacknaylordunn@icloud.com>
2025-09-12 22:19:50 +0100

    Initial Commit

---
+++ CPR Stat Watch App/CPR_StatApp.swift
@@ -0,0 +1,17 @@
+//
+//  CPR_StatApp.swift
+//  CPR Stat Watch App
+//
+//  Created by Jack Naylor Dunn on 12/09/2025.
+//
+
+import SwiftUI
+
+@main
+struct CPR_Stat_Watch_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ CPR Stat/CPR_StatApp.swift
@@ -0,0 +1,17 @@
+//
+//  CPR_StatApp.swift
+//  CPR Stat
+//
+//  Created by Jack Naylor Dunn on 12/09/2025.
+//
+
+import SwiftUI
+
+@main
+struct CPR_StatApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
